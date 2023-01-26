a4df7f33474a2ab7130cfc791668687e718c3b3a
Nevnet99 <luke@primer.io>
2023-01-26 02:00:15 +0000

    feat: basic setup for the project

---
+++ src/components/templates/RootLayout/RootLayout.tsx
@@ -0,0 +1,36 @@
+import Lenis from "@studio-freight/lenis";
+import { useEffect } from "react";
+
+function RootLayout({ children }) {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+
+    const lenis = new Lenis({
+      duration: 1.2,
+      easing: (t) => Math.min(1, 1.001 - 2 ** (-10 * t)),
+      direction: "vertical",
+      gestureDirection: "vertical",
+      smooth: true,
+      mouseMultiplier: 1,
+      smoothTouch: false,
+      touchMultiplier: 2,
+      infinite: false,
+    });
+
+    // get scroll value
+    lenis.on("scroll", ({ scroll, limit, velocity, direction, progress }) => {
+      // console.log({ scroll, limit, velocity, direction, progress });
+    });
+
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+  }, []);
+
+  return <div>{children}</div>;
+}
+
+export default RootLayout;
